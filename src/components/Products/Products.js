4e32c43958eecf9946ada8acf930058b16eef48d
zeeshanchana95 <zeeshanchana95@gmail.com>
2023-06-23 16:43:34 +0500

    add static state management

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,46 +1,77 @@
 import React, { Component } from "react";
 import "./Products.css";
 
+const products = [
+    {
+      emoji: '🍦',
+      name: 'ice cream',
+      price: 5
+    },
+    {
+      emoji: '🍩',
+      name: 'donuts',
+      price: 2.5,
+    },
+    {
+      emoji: '🍉',
+      name: 'watermelon',
+      price: 4
+    }
+  ];
 export default class Product extends Component {
-  state = {
-    cart: [],
-    total: 0,
-  };
+    state = {
+        cart: [],
+      }
 
-  add = () => {
-    this.setState({
-      cart: ['ice cream'],
-      total: 5
-    })
-  }
+      add = (product) => {
+        this.setState(state => ({
+          cart: [...state.cart, product],
+        }))
+      }
 
-remove = () => {
-    this.setState({
-      cart: []
-    })
-  }
+      currencyOptions = {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      }
 
-  currencyOptions = {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }
+      getTotal = () => {
+        const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price, 0);
+        return total.toLocaleString(undefined, this.currencyOptions)
+      }
 
-  getTotal = () => {
-    return this.state.total.toLocaleString(undefined, this.currencyOptions)
-  }
+      remove = (product) => {
+        this.setState(state => {
+          const cart = [...state.cart];
+          const productIndex = cart.findIndex(p => p.name === product.name);
+          if(productIndex < 0) {
+            return;
+          }
+          cart.splice(productIndex, 1)
+          return ({
+            cart
+          })
+        })
+      }
   render() {
-    return (
+    return(
       <div className="wrapper">
-        <div>Shopping Cart: {this.state.cart.length} total items.</div>
-        <div>Total: {this.getTotal()}</div>
-        <div className="product">
-          <span role="img" aria-label="ice cream">
-            🍦
-          </span>
+        <div>
+          Shopping Cart: {this.state.cart.length} total items.
         </div>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.remove}>Remove</button>
+        <div>Total {this.getTotal()}</div>
+
+        <div>
+          {products.map(product => (
+            <div key={product.name}>
+              <div className="product">
+                <span role="img" aria-label={product.name}>{product.emoji}</span>
+              </div>
+              <button onClick={() => this.add(product)}>Add</button>
+              <button onClick={() => this.remove(product)}>Remove</button>
+            </div>
+          ))}
+        </div>
       </div>
-    );
+    )
   }
 }
